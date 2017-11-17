145e4ab9c7a6dc9d4b31deccc1baea1b20bc05ad
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-11-17 14:30:33 +0100

    feat: add list of friends

--- server/controllers/users.controller.ts
+++ server/controllers/users.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from 'express';
 import * as cors from 'cors';
-import * as request from 'request';
+import * as request from 'request-promise';
 import * as slug from 'slug';
 
 import { User } from '../models/user';
@@ -18,38 +18,37 @@ router.get('/', cors(), (req: Request, res: Response) => {
 });
 
 router.post('/facebook', cors(), (req: Request, res: Response) => {
-  request
-    .get('https://graph.facebook.com/me/?access_token=' + req.body['access_token'])
-    .on('response', response => {
-      response.on('data', data => {
-        let json = JSON.parse(data.toString());
-        User.findOne({ 'social.facebook': json.id }, (err, doc) => {
-          if (doc) {
-            res.json(doc);
-          } else {
-            let user = new User({
-              name: json.name,
-              slug: slug(json.name),
-              social: {
-                facebook: json.id
-              }
-            });
-            user.save((err, doc) => {
-              if (err) {
-                console.log(err);
-                res.status(400).json(doc);
-              }
-              res.json(doc);
-            });
-          }
-        });
-      });
+  let fbUser: any;
+  getFbUser(req.body['access_token'])
+    .then((response: any) => {
+      fbUser = response;
+      return findByFacebookId(fbUser.id);
     })
-    .on('error', err => {
-      res.status(401)
-        .json({
-          message: 'failed to sign ig with facebook.'
+    .then((doc: any) => {
+      if (!doc) {
+        let user = new User({
+          name: fbUser.name,
+          slug: slug(fbUser.name),
+          gender: fbUser.gender,
+          social: {
+            facebook: fbUser.id
+          },
         });
+        return user.save();
+      }
+      return new Promise((resolve, reject) => { resolve(doc); });
+    })
+    .then((doc: any) => {
+      return updateFriends(doc, req.body['access_token']);
+    })
+    .then((doc: any) => {
+      res.json(doc);
+    })
+    .catch((err: any) => {
+      res.status(401).json({
+        message: 'cannot login to facebook.',
+        error: err
+      });
     });
 });
 
@@ -60,3 +59,59 @@ router.get('/:id', (req: Request, res: Response) => {
 });
 
 export default router;
+
+function findByFacebookId(id: string) {
+  return User.findOne({ 'social.facebook' : id });
+}
+
+function getFbUser(fbToken: string) {
+  return request({
+    method: 'GET',
+    uri: 'https://graph.facebook.com/me/?fields=name,gender&access_token=' + fbToken,
+    json: true
+  });
+}
+
+function getFbFriends(fbToken: string) {
+  return request({
+    method: 'GET',
+    uri: 'https://graph.facebook.com/me/friends?access_token=' + fbToken,
+    json: true
+  });
+}
+
+function getFbPicture(fbToken: string) {
+  return request({
+    method: 'GET',
+    uri: 'https://graph.facebook.com/me/picture?access_token=' + fbToken,
+    json: true
+  });
+}
+
+function updateFriends(user: any, token: string) {
+  return getFbFriends(token)
+    .then((response: any) => {
+      return compareFriends(user, response.data);
+    })
+    .then((values: any) => {
+      return new Promise((resolve, reject) => {
+        resolve(user);
+      });
+    })
+    .catch((err: any) => {
+      return err;
+    });
+}
+
+function compareFriends(user: any, fbFriends: Array<Object>) {
+  let promises: any[] = [];
+  fbFriends.forEach((friend: any) => {
+    promises.push(findByFacebookId(friend['id']));
+  });
+  return Promise.all(promises)
+    .then(values => {
+      return User.findOneAndUpdate(
+        { _id: user.id },
+        { friends: values }).populate({ path: 'friends', model: 'User' });
+    });
+}
